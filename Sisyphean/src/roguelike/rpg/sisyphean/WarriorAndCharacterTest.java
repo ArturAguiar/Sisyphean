0c6bcaba7c96670c827b6e3c43964fcb6ad25dec
Willy Lund <whlund15@vt.edu>
2012-11-04 19:10:06 -0500

    Created a test method for each method in character except for the last two methods involving the sprite images.
    NOTE: There are two errors with the methods since we have not yet
    decided on a data structure for the skills the warrior will have.

--- Sisyphean/src/roguelike/rpg/sisyphean/WarriorAndCharacterTest.java
+++ Sisyphean/src/roguelike/rpg/sisyphean/WarriorAndCharacterTest.java
@@ -59,8 +59,6 @@ public class WarriorAndCharacterTest extends TestCase
         newChar.setMaxHealth(100);
 
         assertEquals(50.0f, newChar.getHealth());
-
-
     }
 
     /**
@@ -73,10 +71,117 @@ public class WarriorAndCharacterTest extends TestCase
 
         newChar.setHealth(150);
         assertEquals(120.0f, newChar.getHealth());
+    }
 
+    /**
+     * Tests the max mana (getter and setter) methods.
+     */
+    public void testMaxMana()
+    {
+        assertEquals(100.0f, newChar.getMaxMana());
 
+        newChar.setMaxMana(220);
 
+        assertEquals(220.0f, newChar.getMaxMana());
+        newChar.setMana(110);
+        newChar.setMaxMana(100);
 
+        assertEquals(50.0f, newChar.getMana());
+    }
+
+    /**
+     * Tests the current mana (getter and setter) methods.
+     */
+    public void testCurrentMana()
+    {
+        newChar.setMaxMana(100);
+        assertEquals(100.0f, newChar.getMana());
+
+        newChar.setMana(150);
+        assertEquals(100.0f, newChar.getMana());
+    }
+
+    /**
+     * Tests the max stamina (getter and setter) methods.
+     */
+    public void testMaxStamina()
+    {
+        assertEquals(120.0f, newChar.getMaxStamina());
+
+        newChar.setMaxStamina(220);
+
+        assertEquals(220.0f, newChar.getMaxStamina());
+        newChar.setStamina(110);
+        newChar.setMaxStamina(100);
+
+        assertEquals(50.0f, newChar.getStamina());
+    }
+
+    /**
+     * Tests the current stamina (getter and setter) methods.
+     */
+    public void testCurrentStamina()
+    {
+        newChar.setMaxStamina(100);
+        assertEquals(100.0f, newChar.getStamina());
+
+        newChar.setStamina(150);
+        assertEquals(100.0f, newChar.getStamina());
+    }
+
+    /**
+     * Tests the strength (getter and setter) methods.
+     */
+    public void testStrength()
+    {
+        assertEquals(30.0f, newChar.getStrength());
+
+        newChar.setStrength(45);
+        assertEquals(45.0f, newChar.getStrength());
+    }
+
+    /**
+     * Tests the defense (getter and setter) methods.
+     */
+    public void testDefense()
+    {
+        assertEquals(25.0f, newChar.getDefense());
+        newChar.setDefense(40);
+        assertEquals(40.0f, newChar.getDefense());
+    }
+
+    /**
+     * Tests the dexterity (getter and setter) methods.
+     */
+    public void testDexterity()
+    {
+        assertEquals(12.0f, newChar.getDexterity());
+        newChar.setDexterity(20);
+        assertEquals(20.0f, newChar.getDexterity());
+    }
+
+    /**
+     * Tests the intelligence (getter and setter) methods.
+     */
+    public void testIntelligence()
+    {
+        assertEquals(10.0f, newChar.getIntelligence());
+        newChar.setIntelligence(20);
+        assertEquals(20.0f, newChar.getIntelligence());
+    }
+
+    /**
+     * Tests the skills set of the warrior.
+     */
+    public void testGetSkills()
+    {
+        assertEquals(0, newChar.getSkills().size());
+
+        newChar.levelUp();
+        newChar.levelUp();
+
+        assertEquals(1, newChar.getSkills().size());
 
     }
 }
+
