59b3fa61e83e852ca7c06b4f17c7732349ea281d
Artur Aguiar <arturaguiar@hotmail.com>
2012-11-06 22:22:49 -0500

    Changed the MainMenuScreen class to correctly display the menu.

--- Sisyphean/src/roguelike/rpg/sisyphean/MainMenuScreen.java
+++ Sisyphean/src/roguelike/rpg/sisyphean/MainMenuScreen.java
@@ -1,8 +1,6 @@
 package roguelike.rpg.sisyphean;
 
-import android.view.MotionEvent;
-import android.graphics.PointF;
-import sofia.app.ShapeScreen;
+import sofia.app.Screen;
 
 /**
  * // -------------------------------------------------------------------------
@@ -12,54 +10,13 @@ import sofia.app.ShapeScreen;
  *  @author
  *  @version 2012.11.04
  */
-public class MainMenuScreen extends ShapeScreen
+public class MainMenuScreen extends Screen
 {
-    // I found out why it's not loading the xml layout for this.
-    // It needs to extend Screen and not ShapeScreen.
-    // But until we have a maze screen, I need something to test the sprites on. =/
-
-    // Also, for some reason the sprites are kinda broken when I test with the virtual device,
-    // but work perfectly on my phone. =/
-    // Will have to check this with Tony...
-
-    // Everything you see here is probably just temporary, so feel free to change anything you need.
-
-    private GameWorld gameWorld;
-
-    private LogicThread logicThread;
-
-    private Player player;
-
+    /**
+     * This method gets called only once when the screen is created.
+     */
     public void initialize()
     {
-        // This is just for testing, the world should not be created on this screen in the final version.
-        gameWorld = new GameWorld();
-
-        logicThread = new LogicThread(gameWorld);
-        logicThread.setRunning(true);
-        logicThread.start();
-
-        // This is just a test of overlapping the character "naked" image with
-        // the armor image. It works!
-        player = new Warrior("Jack", 30.0f, 30.0f);
-        this.add(player.getMazeSprite());
-        this.add(player.getArmor().getMazeSprite());
-        gameWorld.getAllCharacters().add(player);
-    }
-
-    /**
-     * The method that gets called when the user touches the screen.
-     * @param event The event containing the touch information.
-     */
-    public void onTouchDown(MotionEvent event)
-    {
-        player.moveBy(0.0f, 30.0f);
-    }
-
-    @Override
-    public void onDestroy()
-    {
-        super.onDestroy();
-        logicThread.setRunning(false);
+        // Logic here.
     }
 }
