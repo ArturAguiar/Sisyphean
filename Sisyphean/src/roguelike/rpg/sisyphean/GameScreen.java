6b6101315b99a507da8f362d9f28efeb041812bc
Artur Aguiar <arturaguiar@hotmail.com>
2012-11-08 16:35:16 -0500

    The screens switch now. The GameScreen class was added (TK).

---
+++ Sisyphean/src/roguelike/rpg/sisyphean/GameScreen.java
@@ -0,0 +1,13 @@
+package roguelike.rpg.sisyphean;
+
+import sofia.app.ShapeScreen;
+
+public class GameScreen extends ShapeScreen
+{
+
+    public void initalize()
+    {
+        // Stuff here.
+    }
+
+}
