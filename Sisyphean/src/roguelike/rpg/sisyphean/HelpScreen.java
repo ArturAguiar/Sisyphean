955c509fd6e3706ac03a1de1680101c097ec8cbc
theordinaryhero <tks2015@vt.edu>
2012-11-09 11:00:28 -0500

    I made an instruction screen and buttons take you there now.

---
+++ Sisyphean/src/roguelike/rpg/sisyphean/HelpScreen.java
@@ -0,0 +1,39 @@
+package roguelike.rpg.sisyphean;
+
+import android.widget.Button;
+import sofia.app.Screen;
+
+// -------------------------------------------------------------------------
+/**
+ *  Write a one-sentence summary of your class here.
+ *  Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ *
+ *  @author Tk
+ *  @version Nov 9, 2012
+ */
+public class HelpScreen extends Screen
+{
+
+    private Button help;
+
+    // ----------------------------------------------------------
+    /**
+     * Place a description of your method here.
+     */
+    public void initalize()
+    {
+
+    }
+
+    // ----------------------------------------------------------
+    /**
+     * Place a description of your method here.
+     */
+    public void helpClicked()
+    {
+        presentScreen(GameScreen.class);
+        finish();
+    }
+
+}
--- Sisyphean/src/roguelike/rpg/sisyphean/MainMenuScreen.java
+++ Sisyphean/src/roguelike/rpg/sisyphean/MainMenuScreen.java
@@ -27,4 +27,9 @@ public class MainMenuScreen extends Screen
     {
         presentScreen(GameScreen.class);
     }
+
+    public void helpClicked()
+    {
+        presentScreen(HelpScreen.class);
+    }
 }
